0bfd022b5ee96ab5c686dcc5b5d7c244ea554633
Maria Borodina <linda.en.casa@gmail.com>
2021-11-13 21:32:31 +0300

    added some react routing

--- courses-app-1/src/App.jsx
+++ courses-app-1/src/App.jsx
@@ -4,6 +4,9 @@ import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
 import theme from './theme';
 import {mockedCoursesList, mockedAuthorsList} from './mockData/data';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
 
 function mapCourseToAuthors (course) {
   course.authorList = course.authors.map(authorId => mockedAuthorsList.find(author => author.id === authorId)?.name).join(", ");
@@ -18,10 +21,29 @@ const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-    <div className="App">
-      <Header individuumName='Dave' />        
-      <Courses items={courses} />
-    </div>
+      <Router> 
+        <Switch> 
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/courses">
+            <div className="App">
+              <Header individuumName='Dave' /> 
+              <Courses items={courses} />
+            </div>
+          </Route>
+          <Route exact path="/registration">
+            <Registration />
+          </Route>
+           <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="*">
+            <Login />
+          </Route> 
+
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
